83c6e840aba5f25f8e29da8678ee2c1eff6fd6df
mueen ali <hejiy2002@gmail.com>
2026-01-05 23:59:37 +0300

    adding Controller for profile to update that image

---
+++ src/main/java/com/example/novie/controller/UserProfileController.java
@@ -0,0 +1,42 @@
+package com.example.novie.controller;
+
+import com.example.novie.model.UserProfile;
+import com.example.novie.repository.UserProfileRepository;
+import com.example.novie.service.UserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/profile")
+@RequiredArgsConstructor
+public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+   private UserProfileRepository userProfileRepository;
+
+    @PutMapping("/{profileId}/image")
+    public ResponseEntity<?> updateProfileImage(
+            @PathVariable Long profileId,
+            @RequestParam("image") MultipartFile image
+    ) {
+        try {
+            UserProfile updatedProfile =
+                    userProfileService.updateProfileImage(profileId, image);
+
+            return ResponseEntity.ok("Profile image updated successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @GetMapping("/{profileId}/image")
+    public ResponseEntity<byte[]> getProfileImage(@PathVariable Long profileId) {
+        UserProfile profile = userProfileRepository.findById(profileId)
+                .orElseThrow();
+
+        return ResponseEntity.ok()
+                .header("Content-Type", profile.getImageType())
+                .body(profile.getImageData());
+    }
+}
