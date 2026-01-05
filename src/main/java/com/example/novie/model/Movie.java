36316a0c56b0ac2f8b81274bea639b020c2782f1
mueen ali <hejiy2002@gmail.com>
2026-01-06 00:06:49 +0300

    remove commenting the models

--- src/main/java/com/example/novie/model/Movie.java
+++ src/main/java/com/example/novie/model/Movie.java
@@ -1,46 +1,46 @@
-//package com.example.novie.model;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import jakarta.persistence.*;
-//import lombok.*;
-//import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.UpdateTimestamp;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.time.Year;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@ToString
-//@Entity
-//@EntityListeners(AuditingEntityListener.class)
-//@Table(name = "movies")
-//@Getter
-//@Setter
-//public class Movie {
-//
-//    // movieId
-//    @Id
-//    @Column
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    // Name
-//    @Column
-//    private String name;
-//
-//    @Column
-//    private String description;
-//
-//    @Column
-//    private Year year;
-//
-//    // CategoryId
-//    @ManyToOne
-//    @JoinColumn(name = "category_id")
-//    @JsonIgnore
-//    private Category category;
-//
-//}
+package com.example.novie.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Year;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "movies")
+@Getter
+@Setter
+public class Movie {
+
+    // movieId
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Name
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private Year year;
+
+    // CategoryId
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @JsonIgnore
+    private Category category;
+
+}
--- src/main/java/com/example/novie/model/Tracking.java
+++ src/main/java/com/example/novie/model/Tracking.java
@@ -1,38 +1,38 @@
-//package com.example.novie.model;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import jakarta.persistence.*;
-//import lombok.*;
-//import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.UpdateTimestamp;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//
-//import java.time.LocalDateTime;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@ToString
-//@Entity
-//@EntityListeners(AuditingEntityListener.class)
-//@Table(name = "tracking")
-//@Getter
-//@Setter
-//public class Tracking {
-//
-//    // trackingId
-//    @Id
-//    @Column
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    // Status
-//    @Column
-//    private String status;
-//
-//    // MovieId
-//    @ManyToMany
-//    @JoinColumn(name = "movie_id")
-//    @JsonIgnore
-//    private Movie movie;
-//
-//}
+package com.example.novie.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tracking")
+@Getter
+@Setter
+public class Tracking {
+
+    // trackingId
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Status
+    @Column
+    private String status;
+
+    // MovieId
+    @ManyToMany
+    @JoinColumn(name = "movie_id")
+    @JsonIgnore
+    private Movie movie;
+
+}
