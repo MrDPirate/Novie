94d3fa5326924f4568382c9b37d5f2601516d7ea
mueen ali <hejiy2002@gmail.com>
2026-01-06 21:40:55 +0300

    adding some files foe email verf

---
+++ src/main/java/com/example/novie/mailing/AccountVerificationEmailContext.java
@@ -0,0 +1,38 @@
+package com.example.novie.mailing;
+
+import com.example.novie.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+public class AccountVerificationEmailContext extends AbstractEmailContext {
+    private String token;
+    @Value("${spring.mail.from}")
+    private String fromEmail;
+    @Override
+    public <T> void init(T context){
+        System.out.println("email entered ------>" + fromEmail);
+        User user = (User) context;
+        put("username", user.getUserName());
+        setTemplateLocation("mailing/email-verification");
+        setSubject("Complete your registration");
+        setFrom(fromEmail);
+        setTo(user.getEmailAddress());
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+        put("token", token);
+    }
+
+    public void buildVerificationUrl(final String baseURL, final String token) {
+        final String url = UriComponentsBuilder
+                .fromUriString(baseURL)
+                .path("/auth/users/register/verify")
+                .queryParam("token", token)
+                .toUriString();
+
+        put("verificationURL", url);
+    }
+
+}
--- src/main/java/com/example/novie/model/SecureToken.java
+++ src/main/java/com/example/novie/model/SecureToken.java
@@ -8,21 +8,14 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "secureTokens")
 public class SecureToken {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
     private String token;
-
-    @Column(updatable = false)
-    @Basic(optional = false)
-    private LocalDateTime expiredAt;
-
+    private LocalDateTime expireAt;
     @ManyToOne
-    @JoinColumn(name = "user_id" ,referencedColumnName = "user")
+    @JoinColumn(name = "user_id")
     private User user;
+
 }
