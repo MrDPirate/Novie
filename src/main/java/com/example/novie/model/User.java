da099be4366251c8aa40b6947313442524e0ce7b
MrDPirate <khalil.ak.bh1170@gmail.com>
2026-01-12 19:42:17 +0300

    fixed database not creating a column for verify

--- src/main/java/com/example/novie/model/User.java
+++ src/main/java/com/example/novie/model/User.java
@@ -47,6 +47,7 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews = new ArrayList<>();
 
+    @Column
     private boolean accountVerified;
 
 
